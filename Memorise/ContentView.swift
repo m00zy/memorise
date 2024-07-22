bc8c2b9c5a50e4c9b0fe5bc6d6a2c2d609066c23
m00zy <maxwellzhu123@gmail.com>
2024-07-22 17:19:31 +1200

    Toggleable isFaceUp

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -1,38 +1,40 @@
-////
-////  ContentView.swift
-////  Memorise
-////
-////  Created by Maxwell Zhu on 17/07/2024.
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
             CardView()
             CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = false
+
+    var body: some View {
+        let base = RoundedRectangle(cornerRadius: 12)
+        ZStack(alignment: .center) {
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
 
-struct CardView: View{
-    var body: some View{
-        ZStack {
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-            Text("😂")
-                .padding()
-                .font(.largeTitle)
-                .foregroundColor(.orange)
-        }
-    }
-    
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-//
-//
-//
-
