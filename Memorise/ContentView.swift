0a077648a20ca145848c446b2bb970cf76698373
m00zy <maxwellzhu123@gmail.com>
2024-07-22 19:05:45 +1200

    Removed all code related to the plus and minus buttons at the bottom of the screen

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -9,8 +9,6 @@ struct ContentView: View {
             ScrollView{
                 Cards
             }
-            Spacer()
-            CardCountAdjusters
         }
         .padding()
     }
@@ -23,60 +21,33 @@ struct ContentView: View {
         }
         .foregroundColor(.orange)
     }
+    
+    struct CardView: View {
+        let content: String
+        @State var isFaceUp = false
         
-    var CardCountAdjusters: some View {
-        HStack{
-            CardAdder
-            Spacer()
-            CardRemover
-        }
-            .imageScale(.large)
-            .font(.largeTitle)
-        }
-    
-    func cardCountAdjuster(by offset : Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var CardAdder: some View {
-        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
-    }
-    
-    var CardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-}
-
-struct CardView: View {
-    let content: String
-    @State var isFaceUp = false
-    
-    var body: some View {
-        let base = RoundedRectangle(cornerRadius: 12)
-        ZStack(alignment: .center) {
-            Group{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
+        var body: some View {
+            let base = RoundedRectangle(cornerRadius: 12)
+            ZStack(alignment: .center) {
+                Group{
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                }
+                .opacity(isFaceUp ? 1 : 0)
+                
+                base.fill().opacity(isFaceUp ? 0 : 1)
             }
-            .opacity(isFaceUp ? 1 : 0)
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
             
-            base.fill().opacity(isFaceUp ? 0 : 1)
         }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
-        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
