3156c9790b13df2e5b54b49ffb5f1555d25de1f9
m00zy <maxwellzhu123@gmail.com>
2024-07-22 15:50:20 +1200

    Initial commit
    CardView
    IsFaceUp

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -1,61 +1,38 @@
-//
-//  ContentView.swift
-//  Memorise
-//
-//  Created by Maxwell Zhu on 17/07/2024.
-//
-
+////
+////  ContentView.swift
+////  Memorise
+////
+////  Created by Maxwell Zhu on 17/07/2024.
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+        HStack {
+            CardView()
+            CardView()
         }
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+struct CardView: View{
+    var body: some View{
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .strokeBorder(lineWidth: 2)
+            Text("😂")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+        }
+    }
+    
 }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+//
+//
+//
+
